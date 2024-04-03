3502eb2da723f93f1200f1e384d664fddb9a13d4
harkairt <32367206+harkairt@users.noreply.github.com>
2024-04-03 21:41:29 +0200

    chore: add doc

--- lib/src/hooks/use_memoized_2.dart
+++ lib/src/hooks/use_memoized_2.dart
@@ -2,6 +2,9 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:v_flutter_core/src/hooks/use_keys.dart';
 
+/// Similar to [useMemoized] but it also receives an additional array of [Listenable] objects.
+/// When any of the supplied listenable notifies then it will mark the caller [HookWidget]
+/// as needing a build.
 R useMemoized2<R>(
   R Function() valueBuilder, [
   List<Object?>? keys,
