ab62df6780c04253802accb092e7843e7ec427ce
harkairt <32367206+harkairt@users.noreply.github.com>
2024-04-30 12:14:53 +0200

    fix: streamOf & streamOfAsyncData typing

--- lib/src/extensions/ref_extensions.dart
+++ lib/src/extensions/ref_extensions.dart
@@ -3,26 +3,25 @@ import 'dart:async';
 import 'package:riverpod/riverpod.dart';
 
 extension CoreRefExtensions on Ref<dynamic> {
-  /// Returns a Stream of the values provided by [provided]. Especially useful to get the values of a provider, but
+  /// Returns a Stream of [AsyncData] values provided by [provider] (which provides items of [AsyncValue<T>] type). 
+  /// Especially useful to get the values of a provider, but
   /// do not rebuild [this] when the dependency rebuilds. ([ref.listen] is used internally)
-  Stream<T> streamOf<T>(AlwaysAliveProviderListenable<dynamic> provider) {
+  Stream<T> streamOfAsyncData<T>(AlwaysAliveProviderListenable<AsyncValue<T>> provider) {
     final StreamController<T> controller = StreamController();
 
-    listen(provider, (previous, next) {
-      if (next is AsyncValue<T>) {
-        next.whenData((value) => controller.add(value));
-        return;
-      }
+    listen(provider, (_, next) => next.whenData((value) => controller.add(value)));
+    onDispose(() => controller.close());
 
-      if (next is T) {
-        controller.add(next);
-        return;
-      }
+    return controller.stream;
+  }
 
-      throw ArgumentError(
-        'ref.streamOf was given a provider that does not provide [AsyncValue]$T, but ${next.runtimeType}',
-      );
-    });
+  /// Returns a Stream of the values provided by [provider] (which provides items of [T] type). 
+  /// Especially useful to get the values of a provider, but
+  /// do not rebuild [this] when the dependency rebuilds. ([ref.listen] is used internally)
+  Stream<T> streamOf<T>(AlwaysAliveProviderListenable<T> provider) {
+    final StreamController<T> controller = StreamController();
+
+    listen(provider, (_, next) => controller.add(next));
     onDispose(() => controller.close());
 
     return controller.stream;
@@ -30,26 +29,25 @@ extension CoreRefExtensions on Ref<dynamic> {
 }
 
 extension CoreAutoDisposeRefExtensions on AutoDisposeRef<dynamic> {
-  /// Returns a Stream of the values provided by [provided]. Especially useful to get the values of a provider, but
+  /// Returns a Stream of [AsyncData] values provided by [provider] (which provides items of [AsyncValue<T>] type).
+  /// Especially useful to get the values of a provider, but
   /// do not rebuild [this] when the dependency rebuilds. ([ref.listen] is used internally)
-  Stream<T> streamOf<T>(ProviderListenable<dynamic> provider) {
+  Stream<T> streamOfAsyncData<T>(ProviderListenable<AsyncValue<T>> provider) {
     final StreamController<T> controller = StreamController();
 
-    listen(provider, (previous, next) {
-      if (next is AsyncValue<T>) {
-        next.whenData((value) => controller.add(value));
-        return;
-      }
+    listen(provider, (_, next) => next.whenData((value) => controller.add(value)));
+    onDispose(() => controller.close());
 
-      if (next is T) {
-        controller.add(next);
-        return;
-      }
+    return controller.stream;
+  }
 
-      throw ArgumentError(
-        'ref.streamOf was given a provider that does not provide [AsyncValue]$T, but ${next.runtimeType}',
-      );
-    });
+  /// Returns a Stream of the values provided by [provider] (which provides items of [T] type).
+  /// Especially useful to get the values of a provider, but
+  /// do not rebuild [this] when the dependency rebuilds. ([ref.listen] is used internally)
+  Stream<T> streamOf<T>(ProviderListenable<T> provider) {
+    final StreamController<T> controller = StreamController();
+
+    listen(provider, (_, next) => controller.add(next));
     onDispose(() => controller.close());
 
     return controller.stream;
