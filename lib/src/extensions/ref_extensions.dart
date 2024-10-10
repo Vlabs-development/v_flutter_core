2accef3b3e6b611354cc7eb6b87bc62b14df999c
harkairt <32367206+harkairt@users.noreply.github.com>
2024-10-11 00:02:38 +0200

    feat: add `onError` callback to `streamOfAsyncData` and `streamOf`

--- lib/src/extensions/ref_extensions.dart
+++ lib/src/extensions/ref_extensions.dart
@@ -6,10 +6,11 @@ extension CoreRefExtensions on Ref<dynamic> {
   /// Returns a Stream of [AsyncData] values provided by [provider] (which provides items of [AsyncValue<T>] type).
   /// Especially useful to get the values of a provider, but
   /// do not rebuild [this] when the dependency rebuilds. ([ref.listen] is used internally)
-Stream<T> streamOfAsyncData<T>(
+  Stream<T> streamOfAsyncData<T>(
     AlwaysAliveProviderListenable<AsyncValue<T>> provider, {
     bool fireImmediately = false,
     bool skipError = true,
+    Function(Object error, StackTrace stackTrace)? onError,
     bool skipLoadingOnReload = false,
     bool skipLoadingOnRefresh = true,
     bool broadcast = false,
@@ -22,7 +23,13 @@ Stream<T> streamOfAsyncData<T>(
         provider,
         (_, next) => next.whenOrNull(
           data: (value) => streamController.add(value),
-          error: (error, stackTrace) => skipError ? null : streamController.addError(error, stackTrace),
+          error: (error, stackTrace) {
+            onError?.call(error, stackTrace);
+
+            if (!skipError) {
+              streamController.addError(error, stackTrace);
+            }
+          },
           skipLoadingOnRefresh: skipLoadingOnRefresh,
           skipLoadingOnReload: skipLoadingOnReload,
         ),
@@ -90,6 +97,7 @@ extension CoreAutoDisposeRefExtensions on AutoDisposeRef<dynamic> {
     ProviderListenable<AsyncValue<T>> provider, {
     bool fireImmediately = false,
     bool skipError = true,
+    Function(Object error, StackTrace stackTrace)? onError,
     bool skipLoadingOnReload = false,
     bool skipLoadingOnRefresh = true,
     bool broadcast = false,
@@ -102,7 +110,13 @@ extension CoreAutoDisposeRefExtensions on AutoDisposeRef<dynamic> {
         provider,
         (_, next) => next.whenOrNull(
           data: (value) => streamController.add(value),
-          error: (error, stackTrace) => skipError ? null : streamController.addError(error, stackTrace),
+          error: (error, stackTrace) {
+            onError?.call(error, stackTrace);
+
+            if (!skipError) {
+              streamController.addError(error, stackTrace);
+            }
+          },
           skipLoadingOnRefresh: skipLoadingOnRefresh,
           skipLoadingOnReload: skipLoadingOnReload,
         ),
