7d59906a5f006539bcb047fe443450e1b36aa72d
harkairt <32367206+harkairt@users.noreply.github.com>
2025-01-17 16:49:44 +0100

    fix: detect DismissiblePortalTarget's barrier scroll differently

--- lib/src/widgets/misc/dismissible_portal_target.dart
+++ lib/src/widgets/misc/dismissible_portal_target.dart
@@ -2,7 +2,7 @@ import 'package:entry/entry.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_portal/flutter_portal.dart';
-import 'package:v_flutter_core/src/hooks/use_scroll_notification_observer.dart';
+import 'package:v_flutter_core/src/hooks/change_notifier_hooks.dart';
 import 'package:v_flutter_core/src/widgets/misc/always_scrollbar.dart';
 
 class DismissiblePortalTarget extends HookWidget {
@@ -53,13 +53,10 @@ class DismissiblePortalTarget extends HookWidget {
                       controller: controller,
                       child: HookBuilder(
                         builder: (context) {
-                          useScrollNotificationObserver(
-                            context,
-                            handler: (notification) {
-                              if (notification is UserScrollNotification) {
-                                onOutsideScroll?.call();
-                              }
-                            },
+                          useOnChangeNotifierValueChanged(
+                            controller,
+                            select: (notifier) => notifier.offset,
+                            onChanged: (offset) => onOutsideScroll?.call(),
                           );
                           return SizedBox(
                             width: MediaQuery.of(context).size.width,
